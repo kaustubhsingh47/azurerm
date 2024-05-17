8ade6674669ea64f7c6cbd72004fbb5d078721c8
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2024-05-17 20:20:47 +0530

    Update main.tf

--- shared_image_gallery/main.tf
+++ shared_image_gallery/main.tf
@@ -9,7 +9,10 @@ resource "azurerm_shared_image_gallery" "sig" {
   sharing {
     permission = "Private"
   }
-}
+
+  softDeletePolicy {
+    isSoftDeleteEnabled = true
+  }
 
   tags                         = local.tags
   
@@ -33,10 +36,16 @@ resource "azurerm_shared_image" "si" {
   os_type             = var.os_type
 
   identifier {
-    publisher = "PublisherName"
-    offer     = "OfferName"
-    sku       = "ExampleSku"
+    publisher = var.indentifier_publisher
+    offer     = var.indentifier_offer
+    sku       = var.indentifier_sku
   }
+
+  disk_types_not_allowed = var.disk_types_not_allowed
+  hyper_v_generation     = var.hyper_v_generation
+  end_of_life_date       = "${timeadd(timestamp(), "17520h")}"
+  eula                   = var.eula
+  specialized            = var.specialized
 
   tags                         = local.tags
   
@@ -52,19 +61,25 @@ resource "azurerm_shared_image" "si" {
 }
 
 
-resource "azurerm_shared_image_version" "example" {
+resource "azurerm_shared_image_version" "siv" {
   name                = "0.0.1"
   gallery_name        = azurerm_shared_image_gallery.sig.name
   image_name          = azurerm_shared_image.si.name
   resource_group_name = azurerm_shared_image.rg.resource_group_name
   location            = azurerm_shared_image.rg.location
-  managed_image_id    = azurerm_image.si.azurerm_shared_image.id
+  managed_image_id    = azurerm_image.si.id
 
   target_region {
-    name                   = azurerm_shared_image.si.location
-    regional_replica_count = 5
-    storage_account_type   = "Standard_LRS"
+    name                         = azurerm_shared_image.si.location
+    regional_replica_count       = var.regional_replica_count
+    storage_account_type         = var.storage_account_type
+    disk_encryption_set_id       = var.disk_encryption_set_id
+    exclude_from_latest_enabled  = var.exclude_from_latest_enabled
   }
+
+  end_of_life_date         = formatdate("YYYY-MM-DD", timeadd(timestamp(), "15d"))
+
+
 
   tags                         = local.tags
   
